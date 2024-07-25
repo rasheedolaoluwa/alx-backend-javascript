523f10378be80554ed2177fda5806861a11081a8
Rasheed Olaoluwa <radeyola@gmail.com>
2024-07-25 11:24:41 +0000

    classroom

---
+++ 0x02-ES6_classes/0-classroom.js
@@ -0,0 +1,12 @@
+/**
+ * Represents a classroom.
+ */
+export default class ClassRoom {
+  /**
+   * Creates a new classroom.
+   * @param {Number} maxStudentsSize - The maximum number of students in the class.
+   */
+  constructor(maxStudentsSize) {
+    this._maxStudentsSize = maxStudentsSize;
+  }
+}
