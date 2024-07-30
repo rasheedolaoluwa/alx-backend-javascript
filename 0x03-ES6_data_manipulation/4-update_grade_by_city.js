0fa9567018770b224163780aaf441cca5e2fab1d
Rasheed Olaoluwa <radeyola@gmail.com>
2024-07-30 14:42:00 +0000

    fix lint error task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -2,8 +2,10 @@
  * Updates the grades of a list of students in a given city.
  * @param {{id: Number, firstName: String, location: String}[]} students - The list of students.
  * @param {String} city - The city of students.
- * @param {{studentId: Number, grade: Number}[]} newGrades - The new grades to be given to a student.
- * @returns {{id: Number, firstName: String, location: String, grade: Number|string}[]} The updated list of students with grades.
+ * @param {{studentId: Number, grade: Number}[]} newGrades -
+ * The new grades to be given to a student.
+ * @returns {{id: Number, firstName: String, location: String, grade: Number|string}[]}
+ * The updated list of students with grades.
  */
 export default function updateStudentGradeByCity(students, city, newGrades) {
   const defaultGrade = { grade: 'N/A' };
