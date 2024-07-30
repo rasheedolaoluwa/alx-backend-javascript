e81659076592d3c2e869ce84d04b829bc61227be
Rasheed Olaoluwa <radeyola@gmail.com>
2024-07-30 14:38:10 +0000

    fix lint error task 2

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -2,7 +2,8 @@
  * Retrieves students in a given location.
  * @param {{id: Number, firstName: String, location: String}[]} students - The list of students.
  * @param {String} city - The location.
- * @returns {{id: Number, firstName: String, location: String}[]} The list of students in the given location.
+ * @returns {{id: Number, firstName: String, location: String}[]}
+ * The list of students in the given location.
  */
 export default function getStudentsByLocation(students, city) {
   if (students instanceof Array) {
