8e3fa913757e04b2e9ef6d465c0db2e2242dd00d
Rasheed Olaoluwa <radeyola@gmail.com>
2024-07-30 14:43:07 +0000

    set

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,8 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @returns {Set} The created set.
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
