c88b55e381f0bbe6eabc96439f13eafa4212c3aa
rasheed <radeyola@gmail.com>
2024-09-18 18:56:00 +0000

    console.js

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Logs a message to the console.
+ * @param {String} msg The message to log.
+ */
+const displayMessage = (msg) => {
+  console.log(msg);
+};
+
+module.exports = displayMessage;
